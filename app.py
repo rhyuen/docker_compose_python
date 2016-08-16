e710707c45c1ab63b68e4844639a7d2db6683956
Robert <dungeonry@hotmail.com>
2016-08-15 18:53:26 -0700

    There were some syntax errors in the python prog.

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 redis = Redis(host="redis", port = 6379)
 
 @app.route("/")
-def hello()
+def hello():
   redis.incr("hits")
   return "Hello World! I have been seen %s times." % redis.get("hits")
 
-if __name__ = "__main__"
+if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
